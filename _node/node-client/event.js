a79cbe87b3a5419bfae6829be8aadd8bc1e83733
dev.thapa <dev@miracleinterface.com>
2018-02-13 11:36:48 +0545

    [FIX] - updated

--- _node/node-client/event.js
+++ _node/node-client/event.js
@@ -1,1 +1,14 @@
-112313123123
+function Event(client) {
+    $('td').click(function() {
+        var clsName = this.className;
+        onTdClick(clsName, client);
+    });
+};
+
+function onTdClick(clsName, client) {
+	if(client.userIcon[client.user_name].currentDigit == client.currentMove){
+	    client.nodeClient.emit('onTdClick', clsName);
+	}else{
+		console.log('Not Your Turn');
+	}
+}
